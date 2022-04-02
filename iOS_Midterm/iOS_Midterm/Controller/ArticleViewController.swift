4fc956107f1fc0d962a1cb78488ec1236e9f4b47
Jenny Hung <Jenny.PJ.Hung@gmail.com>
2022-04-02 15:13:38 +0800

    Modify textField and Label layout

--- iOS_Midterm/iOS_Midterm/Controller/ArticleViewController.swift
+++ iOS_Midterm/iOS_Midterm/Controller/ArticleViewController.swift
@@ -13,12 +13,27 @@ class ArticleViewController: UIViewController {
     
     @IBOutlet weak var inputCategoryTextField: UITextField!
     
-    @IBOutlet weak var inputContentTextField: UITextField!
+    @IBOutlet weak var inputContentTextView: UITextView!
     
     var publisherManager = PublisherManager()
     
+    var placeholder = UILabel()
+    
+    var donebtn = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        inputTitleTextField.delegate = self
+        
+        inputCategoryTextField.delegate = self
+
+        inputContentTextView.layer.borderColor = UIColor.systemGray5.cgColor
+        
+        inputContentTextView.layer.borderWidth = 1
+        
+        inputContentTextView.layer.cornerRadius = 5
+        
 
     }
     
@@ -27,7 +42,7 @@ class ArticleViewController: UIViewController {
         
         guard let inputTitle = inputTitleTextField.text,
               let inputCategory = inputCategoryTextField.text,
-              let inputContent = inputContentTextField.text
+              let inputContent = inputContentTextView.text
         else { return }
         
         if inputTitle == "" || inputCategory == "" || inputContent == "" {
@@ -49,3 +64,36 @@ class ArticleViewController: UIViewController {
     }
     
 }
+
+
+extension ArticleViewController: UITextFieldDelegate {
+    
+    // 當點擊 view 任何一處時，鍵盤收起
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+
+    }
+    
+
+    // 前兩個 TextField 改成顯示 Next，並且可以跳到下一個 TextField
+    // 而最後一個 TextView 則顯示 Return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+       let nextTag = textField.tag + 1
+        
+        if let nextResponder = textField.superview?.viewWithTag(nextTag) {
+            
+            nextResponder.becomeFirstResponder()
+            
+        } else {
+            
+            textField.resignFirstResponder()
+            
+        }
+        
+        return true
+    }
+    
+}
+
